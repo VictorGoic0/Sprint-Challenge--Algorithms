a5024a0019ba7e75595049af3ae094d2e5d39b19
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-06-07 16:56:53 -0400

    Switched statement in robot sort so that it didn't hang on larger tests

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -102,30 +102,20 @@ class SortingRobot:
             while self.can_move_right():
                 self.swap_item()
                 self.move_right()
-                if self.compare_item() == -1:
+                if self.compare_item() == 1:
+                    self.swap_item()
                     self.move_left()
                     self.swap_item()
                     self.move_right()
+                    self.set_light_on() 
                 else:
-                    self.set_light_on()
-                    self.swap_item()
                     self.move_left()
                     self.swap_item()
                     self.move_right()
 
             while self.can_move_left():
-                self.swap_item()
                 self.move_left()
-                if self.compare_item() < -1:
-                    self.set_light_on()
-                    self.swap_item()
-                    self.move_right()
-                    self.swap_item()
-                    self.move_left()
-                else:
-                    self.move_right()
-                    self.swap_item()
-                    self.move_left()
+
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
