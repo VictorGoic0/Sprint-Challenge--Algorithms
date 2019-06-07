ae2649375078baf6819e49284631b1cd65944521
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-06-07 13:58:00 -0400

    Current progress on robot sort

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,9 +96,17 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
-
+        self.set_light_on()
+        while self.light_is_on():
+            self.set_light_off()
+            while self.can_move_right():
+                self.swap_item()
+                self.move_right()
+                if self.compare_item() < 0:
+                    self.move_right()
+                    self.set_light_on()
+                else:
+                    self.swap_item()
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
